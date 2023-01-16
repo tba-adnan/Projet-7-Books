add8b5b3d2c0f5bacff9d4d9f74a56f801eeb97d
TebbaaX <>
2023-01-17 00:30:06 +0100

    Minor add.

--- books-front/src/components/main.js
+++ books-front/src/components/main.js
@@ -4,25 +4,42 @@ import axios from "axios";
 
 
 export default class MainBooks extends React.Component {
-    constructor(){
-        super();
 
-        this.state = {
-            search : ''
-        }
-    }
+constructor(props) {
+    super(props);
+    this.state = {value:'', search:'', books:[]};
+// 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
 
-render() {
-    return(
+  handleSubmit(event) {
+    axios.get("https://www.googleapis.com/books/v1/volumes?q="+this.state.value).then((response) => {this.setState({search : response.data.items}); this.setState({books:response.data.items})});
+    // console.log(this.state.search)
+    console.log(this.state.books)
 
-<div>
-    <h1>Test</h1>
-</div>
+    event.preventDefault();
+  }
 
+  render() {
+    return (
+        <div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
 
+       
+      </div>
     );
-}
+  }
 
 
 }
