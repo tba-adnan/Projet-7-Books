ab6666ec0ef8305024a109c67b7fddc51ca19617
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-01-16 16:58:40 +0100

    initial commit.

---
+++ books-front/src/components/main.js
@@ -0,0 +1,28 @@
+import React from "react";
+import axios from "axios";
+
+
+
+export default class MainBooks extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            search : ''
+        }
+    }
+
+
+render() {
+    return(
+
+<div>
+    <h1>Test</h1>
+</div>
+
+
+    );
+}
+
+
+}
